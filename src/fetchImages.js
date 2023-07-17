2e86ceaaec25b3aa075123ea9e918f618ff94223
va7ul <vas7ul@gmail.com>
2023-07-18 01:41:57 +0300

    Update fetchImages.js

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,31 +1,34 @@
 export async function fetchImages(name, page = 1) {
-    const BASE_URL = 'https://pixabay.com/api/';
-    const KEY = '32844399-402b025363825ff7850242d10';
+  const BASE_URL = 'https://pixabay.com/api/';
+  const API_KEY = '38290749-a2ba3d07cb211d325e34a4328';
 
-    const axios = require('axios').default;
+  const axios = require('axios').default;
 
-    try {
-        return await axios.get(`${BASE_URL}?key=${KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`).then(resp => {
-            if (!resp.data.total) {
-            throw new Error("Bad request!!!");
-            }
+  try {
+    return await axios
+      .get(
+        `${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
+      )
+      .then(resp => {
+        if (!resp.data.total) {
+          throw new Error('Bad request!!!');
+        }
         return resp.data;
-    });
-    } catch (error) {
-        console.log(error);
-    }
+      });
+  } catch (error) {
+    console.log(error);
+  }
 
-
-    // return fetch(`${BASE_URL}?key=${KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`).then(resp => {
-    //     if (!resp.ok) {
-    //         throw new Error(resp.status);
-    //     }
-    //     return resp.json();
-    // }).then(data => {
-    //     if (!data.total) {
-    //         throw new Error(data.total);
-    //     }
-    //     console.log(data);
-    //     return data;
-    // })
+  // return fetch(`${BASE_URL}?key=${KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`).then(resp => {
+  //     if (!resp.ok) {
+  //         throw new Error(resp.status);
+  //     }
+  //     return resp.json();
+  // }).then(data => {
+  //     if (!data.total) {
+  //         throw new Error(data.total);
+  //     }
+  //     console.log(data);
+  //     return data;
+  // })
 }
